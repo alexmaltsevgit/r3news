61b7c942251633150a0f88a6f4e668bbef387a04
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-10-27 09:40:50 +0300

    simplify home main component

--- src/components/layout/HomeMain/HomeMain.component.tsx
+++ src/components/layout/HomeMain/HomeMain.component.tsx
@@ -1,54 +1,16 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import LayoutContainerCss from "../LayoutContainer/LayoutContainer.style";
+import React from "react";
+import { Container, Content } from "./HomeMain.styles";
 import { queryParameters } from "./HomeMain.utils";
-import { Article } from "../../types";
 import ArticleCard from "../../shared/ArticleCard/ArticleCard.component";
-import JSON_EXAMPLE from "../../../json-example";
-import { NewsAPI } from "../../../utils/NewsAPI.query";
-import { Environment } from "../../../utils/Environment";
-
-const Container = styled.main`
-  ${LayoutContainerCss}
-`;
-
-const Content = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-column-gap: 15px;
-  grid-row-gap: 20px;
-`;
+import useArticles from "../../../hooks/useArticles";
 
 const HomeMain = () => {
-  const [news, setNews] = useState<Array<Article>>([]);
-
-  useEffect(() => {
-    const fetchNews = async (url: string) => {
-      const response = await fetch(url);
-      const data = await response.json();
-      setNews(data["articles"]);
-    };
-
-    const fetchFake = () => {
-      // @ts-ignore
-      setNews(JSON_EXAMPLE.articles);
-    };
-
-    const queryBuilder = new NewsAPI.QueryBuilder();
-    queryBuilder.setQueryParameters(queryParameters);
-    const url = queryBuilder.getUrl();
-
-    try {
-      Environment.isDebug() ? fetchFake() : fetchNews(url);
-    } catch (e) {
-      console.error(e);
-    }
-  }, []);
+  const articles = useArticles(queryParameters);
 
   return (
     <Container>
       <Content>
-        {news.map((article) => (
+        {articles.map((article) => (
           <ArticleCard key={article.url} article={article} />
         ))}
       </Content>
